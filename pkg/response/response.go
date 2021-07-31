c1cc80e3499202840c5c9a70683f1f8c5462c907
nebisin <massive00@windowslive.com>
2021-07-31 23:13:04 +0300

    response imp

--- pkg/response/response.go
+++ pkg/response/response.go
@@ -9,6 +9,12 @@ func JSONResponse(w http.ResponseWriter, status int, data interface{}) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 
-	err := json.NewEncoder(w).Encode(data)
+	js, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	_, err = w.Write(js)
+
 	return err
 }
