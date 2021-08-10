1cb623a9b6a3e5175803420fb993b0dd64dd68f8
nebisin <massive00@windowslive.com>
2021-08-10 21:47:15 +0300

    graceful shutdown

---
+++ internal/app/server.go
@@ -0,0 +1,53 @@
+package app
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func (s *server) serve() error {
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", s.config.port),
+		Handler:      s.router,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		IdleTimeout:  time.Minute,
+	}
+
+	shutdownError := make(chan error)
+
+	go func() {
+		quit := make(chan os.Signal, 1)
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+		sign := <-quit
+
+		s.logger.WithField("signal", sign.String()).Info("shutting down the server")
+
+		ctx,cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		shutdownError <- srv.Shutdown(ctx)
+	}()
+
+	s.logger.WithField("port", srv.Addr).Info("starting the server")
+
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <- shutdownError
+	if err != nil {
+		return err
+	}
+
+	s.logger.WithField("addr", srv.Addr).Info("stopped the server")
+
+	return nil
+}
